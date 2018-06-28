74830e0037e6cbb9a3cc8e3e4658c511c97c0ea2
Duy Tai Nguyen <duytai.cse@gmail.com>
2018-06-28 18:34:01 +0800

    fix testcases

--- test/execute.js
+++
@@ -1,8 +0,0 @@
-const shell = require('shelljs')
-const fs = require('fs')
-const { expect } = require('chai')
-
-it('execute', () => {
-  shell.cd('./env')
-  shell.exec('node ../index.js -e 10')
-})
---
+++ test/index.js
@@ -0,0 +1,33 @@
+const shell = require('shelljs')
+const fs = require('fs')
+const { expect } = require('chai')
+
+it('compile', () => {
+  shell.cd('./env')
+  shell.exec('node ../index.js -c')
+  const files = shell.ls('build/*.{abi,bin}')
+  expect(files.length).gt(0)
+  shell.cd('../')
+})
+
+it('generate', () => {
+  shell.cd('./env')
+  shell.exec('node ../index.js -g 100')
+  const abiFiles = shell.ls('build/*.abi')
+  abiFiles.forEach(file => {
+    const abi = JSON.parse(fs.readFileSync(file, 'utf8'))
+    abi.forEach(func => {
+      const { inputs } = func
+      inputs.forEach(({ name, type, values = [] }) => {
+        expect(values.length).to.gt(0)
+      })
+    })
+  })
+  shell.cd('../')
+})
+
+it('execute', () => {
+  shell.cd('./env')
+  shell.exec('node ../index.js -e 1')
+  shell.cd('../')
+})
